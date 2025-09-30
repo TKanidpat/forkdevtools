a61b3a52a31dc04134e2decb2fb27f0fee8bde93
TenKanidpat <141381578+TKanidpat@users.noreply.github.com>
2025-09-30 15:57:27 +0700

    Update main.py

--- 03_Jenkins/week13/03_Github_webhook/main.py
+++ 03_Jenkins/week13/03_Github_webhook/main.py
@@ -12,7 +12,7 @@ async def read_root():
             <title>Welcome</title>
         </head>
         <body>
-            <h1>Welcome to Jenkin Webhook using Fastapi พลอยเหนื่อยแล้ว </h1>
+            <h1>Welcome to Jenkin Webhook using Fastapi พลอยเหนื่อยแล้ว ฟาอัลไปคุยกับคนอื่นเถอะ</h1>
         </body>
     </html>
     """
